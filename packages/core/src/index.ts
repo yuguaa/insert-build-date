c2c2532f5dc4997e4975160ad46bc4c22457bca1
yuguaa <280722781@qq.com>
2023-02-08 14:06:48 +0800

    feat:添加ts配置

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -1,18 +1,19 @@
+import type { Compiler } from "webpack";
+
 class InsertBuildDate {
-  apply(compiler) {
-    compiler.hooks.compilation.tap('InsertBuildDate', compilation => {
-      compilation.hooks.htmlWebpackPluginAfterHtmlProcessing.tap(
-        'InsertBuildDate',
-        data => {
-          data.html = data.html.replace(
-            '<head>',
-            `<head><meta name="build-date" content="${new Date().toLocaleString()}">`
-          );
-        }
-      );
-    });
-  }
+	apply(compiler: Compiler) {
+		compiler.hooks.compilation.tap("InsertBuildDate", (compilation) => {
+			(compilation.hooks as any).htmlWebpackPluginAfterHtmlProcessing.tap(
+				"InsertBuildDate",
+				(data: { html: string }) => {
+					data.html = data.html.replace(
+						"<head>",
+						`<head><meta name="build-date" content="${new Date().toLocaleString()}">`
+					);
+				}
+			);
+		});
+	}
 }
 
-export default InsertBuildDate
-
+export default InsertBuildDate;
